e838e1b4f3903c2c751d6bef7dcb2fd3f7b4f76e
Rahulbaratwaj18 <{rahulbaratwaj2001@gmail.com}>
2023-12-08 13:08:18 +0530

    Added uri in session

--- app.js
+++ app.js
@@ -24,15 +24,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 
-app.use(session({
-  secret: 'Rahul Baratwaj',
-  resave: false,
-  saveUninitialized: true,
-  store:MongoStore.create({
-    mongoUrl : process.env.MONGODB_URI
-  })
-}))
+// app.use(session({
+//   secret: 'Rahul Baratwaj',
+//   resave: false,
+//   saveUninitialized: true,
+//   store:MongoStore.create({
+//     mongoUrl : process.env.MONGODB_URI
+//   })
+// }))
 
+
+app.use(
+  session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      // Provide one of the following options:
+      // 1. mongoUrl: 'mongodb://localhost:27017/your-database-name'
+      // 2. clientPromise: MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true })
+      // 3. client: Your existing MongoClient instance
+      mongoUrl: process.env.MONGODB_URI, // Change this with your MongoDB connection URL
+      // Other optional configurations can be added here
+    }),
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
